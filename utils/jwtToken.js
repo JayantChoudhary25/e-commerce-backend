31dc60b34a2d8805041638889eb154dd2e5237e7
Jay <jayantchoudhary271@gmail.com>
2023-10-25 13:39:03 +0530

    cookie fix

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,6 +9,7 @@ const sendToken = (user, statusCode, res) => {
     maxAge: 72 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true,
+    path: '/',
   };
 
   res.status(statusCode).cookie("token", token, options).json({
