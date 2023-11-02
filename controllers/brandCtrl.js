e9b0e10f506baaac0ac773e59d6b4fae2bbf7eee
Jay <jayantchoudhary271@gmail.com>
2023-11-02 10:03:45 +0530

    Search Filter in brand

--- controllers/brandCtrl.js
+++ controllers/brandCtrl.js
@@ -64,3 +64,21 @@ exports.getallBrand = async (req, res) => {
     throw new Error(error);
   }
 };
+exports.getallBrand = async (req, res) => {
+  try {
+    const searchQuery = req.query.search; // Get the search query from the request query parameters
+
+    // Create a Mongoose query to search for brands
+    const brandQuery = Brand.find();
+
+    // If a search query is provided, use it to filter the results
+    if (searchQuery) {
+      brandQuery.regex("brand", new RegExp(searchQuery, "i")); // Case-insensitive search
+    }
+
+    const brands = await brandQuery.exec();
+    res.json(brands);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
