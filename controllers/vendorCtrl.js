def5741e58bca277c1ceba29f5289b8795054d37
Jay <jayantchoudhary271@gmail.com>
2023-11-03 12:26:56 +0530

    Fix getAllVendors

--- controllers/vendorCtrl.js
+++ controllers/vendorCtrl.js
@@ -24,8 +24,24 @@ exports.deleteVendor = async (req, res) => {
 };
 
 exports.getAllVendors = async (req, res) => {
-  const vendors = await Vendor.find();
-  res.json(vendors);
+  // const vendors = await Vendor.find();
+  // res.json(vendors);
+  try {
+    const searchQuery = req.query.search;
+
+    const brandQuery = Vendor.find();
+
+    if (searchQuery) {
+      brandQuery.regex("vendorName", new RegExp(searchQuery, "i"));
+      brandQuery.regex("companyName", new RegExp(searchQuery, "i"));
+      brandQuery.regex("email", new RegExp(searchQuery, "i"));
+    }
+
+    const vendors = await brandQuery.exec();
+    res.json(vendors);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 exports.getaVendor = async (req, res) => {
