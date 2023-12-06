5f4e455a0bacf14d518edcbda4a25de8ed39f407
Jay <jayantchoudhary271@gmail.com>
2023-12-06 11:14:07 +0530

    Code fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -509,10 +509,14 @@ exports.userCart = async (req, res) => {
 // Increase or decrease count of a product in the cart
 exports.increaseProductCount = async (req, res) => {
   const { productId, color, action } = req.body;
-  const { _id } = req.user._id;
+  const userId = req.user._id;
 
   try {
-    const user = await User.findById(_id);
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
     let existingCart = user.cart;
 
     // Find the index of the product in the cart
@@ -548,7 +552,7 @@ exports.increaseProductCount = async (req, res) => {
       await user.save();
 
       // Update the Cart document if it exists
-      let cartDocument = await Cart.findOne({ orderby: user._id });
+      let cartDocument = await Cart.findOne({ orderby: userId });
 
       if (cartDocument) {
         cartDocument.products = existingCart;
