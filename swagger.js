b15f00a2b594cec70a2b4de2539e8c7337f32539
Jay <jayantchoudhary271@gmail.com>
2023-12-11 17:03:44 +0530

    Swagger Code changes

--- swagger.js
+++ swagger.js
@@ -1,14 +1,24 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
-    info : {
-        title : "My API",
-        description: "E-com"
-    },
-    host: 'localhost:4000'
+  info: {
+    title: "My API",
+    description: "E-com",
+  },
+  host: "localhost:4000",
 };
 
 const outputFile = "./swagger-output.json";
-const routes = ["./routes/auth.js", './routes/product.js'];
+const routes = [
+  "./routes/auth.js",
+  "./routes/product.js",
+  "./routes/sizeChart",
+  "./routes/prodCategory",
+  "./routes/subCategory",
+  "./routes/brand",
+  "./routes/color",
+  "./routes/vendor",
+  "./routes/currency"
+];
 
-swaggerAutogen(outputFile , routes , doc);
+swaggerAutogen(outputFile, routes, doc);
