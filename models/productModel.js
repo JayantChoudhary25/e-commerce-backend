0595b9cbbf32a3035af99104f61a4d4fdbb64ff9
Jay <jayantchoudhary271@gmail.com>
2023-12-12 19:10:58 +0530

    Model fix

--- models/productModel.js
+++ models/productModel.js
@@ -52,33 +52,7 @@ const productSchema = new mongoose.Schema(
     ],
     sizeChart: [
       {
-        size: String, // e.g., 'S', 'M', 'L', etc.
-        measurements: {
-          chest: {
-            unit: String, // e.g., 'inches', 'centimeters', etc.
-            value: Number,
-          },
-          waist: {
-            unit: String,
-            value: Number,
-          },
-          hips: {
-            unit: String,
-            value: Number,
-          },
-          length: {
-            unit: String,
-            value: Number,
-          },
-          sleeveLength: {
-            unit: String,
-            value: Number,
-          },
-          shoulderWidth: {
-            unit: String,
-            value: Number,
-          },
-        },
+        size: String,
       },
     ],
     color: [],
